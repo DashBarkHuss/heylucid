1a65a69a5c40c90dd667091610fb3c0b628e413d
Dashiell Bark-Huss <dashiellbark-huss@dashielusssMBP2.jetpack>
2018-02-19 00:20:01 -0500

    flic route added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :reality_checks
   root 'home#index'
   get 'home/index'
+  get 'flic', to: 'reality_checks#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
