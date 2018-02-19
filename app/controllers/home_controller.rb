876e93d3201e68d298f15d8dcd84913b8972d5c9
Dashiell Bark-Huss <dashiellbark-huss@dashielusssMBP2.jetpack>
2018-02-19 13:38:00 -0500

    formatting, eastern timezone

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @number = 0
+    @reality_checks = RealityCheck.all
+    @reality_checks_today = RealityCheck.where("created_at >= ?", DateTime.now.in_time_zone('Eastern Time (US & Canada)').beginning_of_day)
   end
 end
--- app/controllers/reality_checks_controller.rb
+++ app/controllers/reality_checks_controller.rb
@@ -5,6 +5,7 @@ class RealityChecksController < ApplicationController
   # GET /reality_checks.json
   def index
     @reality_checks = RealityCheck.all
+    @reality_checks_today = RealityCheck.where("created_at >= ?", DateTime.now.in_time_zone('Eastern Time (US & Canada)').beginning_of_day)
   end
 
   # GET /reality_checks/1
