43bcbb1c9d44cfaa37936b9152d5a142b410afc6
Dashiell Bark-Huss <dashiellbark-huss@dashielusssMBP2.jetpack>
2018-02-20 10:25:42 -0500

    fix start of day. if it's before 4 am start of start is yesterday at 4 am

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,10 @@ class HomeController < ApplicationController
     @time_gaps = []
 
     #gets reality checks after 4am on today
+    if DateTime.now.in_time_zone('Eastern Time (US & Canada)').strftime("%H").to_i >= 4
     @reality_checks_today = RealityCheck.where("created_at >= ?", DateTime.now.in_time_zone('Eastern Time (US & Canada)').beginning_of_day + 4.hours)
+  else
+    @reality_checks_today = RealityCheck.where("created_at >= ?", DateTime.now.in_time_zone('Eastern Time (US & Canada)').beginning_of_day - 1.day + 4.hours)
   end
+end
 end
