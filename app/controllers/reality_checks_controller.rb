059389412728e3453ec366bc1e31b35384dc965f
Dashiell Bark-Huss <dashiellbark-huss@dashielusssMBP2.jetpack>
2018-02-20 11:43:17 -0500

    formatting on rc page

--- app/controllers/reality_checks_controller.rb
+++ app/controllers/reality_checks_controller.rb
@@ -4,9 +4,25 @@ class RealityChecksController < ApplicationController
   # GET /reality_checks
   # GET /reality_checks.json
   def index
+    @number = 0
     @reality_checks = RealityCheck.all
-    @reality_checks_today = RealityCheck.where("created_at >= ?", DateTime.now.in_time_zone('Eastern Time (US & Canada)').beginning_of_day)
-  end
+    @time_gaps = []
+    @timezone = 'Eastern Time (US & Canada)'
+    @today = DateTime.now.in_time_zone(@time_zone)
+
+    #sets start_of_day differently if its BEFORE or AFTER 4am______ eventually have start time specified by user. This is for if the user is a night owl, their reality checks after 12 am will count as the same day
+    @start_time = 4 #4am
+    if @today.strftime("%H").to_i >= @start_time
+    @same_day = true
+    @start_of_day = @today.beginning_of_day + @start_time.hours
+    else
+    @same_day = false
+    @start_of_day = @today.beginning_of_day - 1.day + @start_time.hours
+    end
+
+    @yesterday = @start_of_day - 1.day
+    @reality_checks_today = RealityCheck.where("created_at >= ?", @start_of_day)
+    @yesterdays_reality_checks = RealityCheck.where(created_at: @yesterday..@start_of_day)  end
 
   # GET /reality_checks/1
   # GET /reality_checks/1.json
