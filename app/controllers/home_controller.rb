86ac5c8a695e8e1d096f9b30388da632ed1ebcbd
Dashiell Bark-Huss <dashiellbark-huss@dashielusssMBP2.jetpack>
2018-02-20 10:06:34 -0500

    fixed bug. conditional to only find time gaps if there are more than one rc's

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     @start_time = Time.zone.parse('2018-02-19 11:08:32')
     @number = 0
     @reality_checks = RealityCheck.all
+    @time_gaps = []
 
     #gets reality checks after 4am on today
     @reality_checks_today = RealityCheck.where("created_at >= ?", DateTime.now.in_time_zone('Eastern Time (US & Canada)').beginning_of_day + 4.hours)
