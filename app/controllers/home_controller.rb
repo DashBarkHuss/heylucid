1a92baeeb260e041c5e0cb647ff3f65ca121c006
Dashiell Bark-Huss <dashiellbark-huss@dashielusssMBP2.jetpack>
2018-02-20 11:30:14 -0500

    DRY and formatting time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,24 @@
 class HomeController < ApplicationController
   def index
-    @start_time = Time.zone.parse('2018-02-19 11:08:32')
     @number = 0
     @reality_checks = RealityCheck.all
     @time_gaps = []
+    @timezone = 'Eastern Time (US & Canada)'
+    @today = DateTime.now.in_time_zone(@time_zone)
 
-    #gets reality checks after 4am on today
-    if DateTime.now.in_time_zone('Eastern Time (US & Canada)').strftime("%H").to_i >= 4
-    @reality_checks_today = RealityCheck.where("created_at >= ?", DateTime.now.in_time_zone('Eastern Time (US & Canada)').beginning_of_day + 4.hours)
-  else
-    @reality_checks_today = RealityCheck.where("created_at >= ?", DateTime.now.in_time_zone('Eastern Time (US & Canada)').beginning_of_day - 1.day + 4.hours)
+    #sets start_of_day differently if its BEFORE or AFTER 4am______ eventually have start time specified by user. This is for if the user is a night owl, their reality checks after 12 am will count as the same day
+    @start_time = 4 #4am
+    if @today.strftime("%H").to_i >= @start_time
+    @same_day = true
+    @start_of_day = @today.beginning_of_day + @start_time.hours
+    else
+    @same_day = false
+    @start_of_day = @today.beginning_of_day - 1.day + @start_time.hours
+    end
+
+    @yesterday = @start_of_day - 1.day
+    @reality_checks_today = RealityCheck.where("created_at >= ?", @start_of_day)
+    @yesterdays_reality_checks = RealityCheck.where(created_at: @yesterday..@start_of_day)
+
   end
-end
 end
