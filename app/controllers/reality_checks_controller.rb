8ca551364aecbb0995361d6d6a43c4866b26d23a
Dashiell Bark-Huss <dashiellbark-huss@dashielusssMBP2.jetpack>
2018-02-20 12:06:53 -0500

    checking time issue

--- app/controllers/reality_checks_controller.rb
+++ app/controllers/reality_checks_controller.rb
@@ -11,7 +11,7 @@ class RealityChecksController < ApplicationController
     @today = DateTime.now.in_time_zone(@time_zone)
 
     #sets start_of_day differently if its BEFORE or AFTER 4am______ eventually have start time specified by user. This is for if the user is a night owl, their reality checks after 12 am will count as the same day
-    @start_time = 4 #4am
+    @start_time = 0 #4am
     if @today.strftime("%H").to_i >= @start_time
     @same_day = true
     @start_of_day = @today.beginning_of_day + @start_time.hours
